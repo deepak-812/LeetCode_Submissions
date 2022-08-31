d51c76c6ce1a4d24bff5f1f27574db02be51bac8
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2022-08-31 23:42:26 +0530

    Cpp_solution

---
+++ 116-populating-next-right-pointers-in-each-node/Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution { // BFS based Solution Iterative Version
+public:
+    Node* connect(Node* root) {  // T.C. O(n) A.S. O(W+1) W is max-width of Tree
+        if(!root){
+            return root;
+        }
+
+        queue<Node*> q;
+        q.push(root);
+        Node* curr=NULL;
+        while(q.empty()==false){
+            int sze=q.size();
+            for(int i=0;i<sze;i++){
+                Node* curr=q.front();
+                if(q.front()->left){
+                    q.push(q.front()->left);
+                    q.push(q.front()->right);
+                }
+                if(i==sze-1){
+                    curr->next=NULL;
+                }
+                q.pop();
+                if(i<sze-1){
+                    curr->next=q.front();
+                }
+            }
+        }
+       return root;
+    }
+};
