fefa48b461b42f41dac867ea14d7b240007ca3db
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2022-08-31 23:43:02 +0530

    solution

---
+++ 117-populating-next-right-pointers-in-each-node-ii/Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {  // Iterative Solution T.C.  O(n)  A.S. O(w+1)
+public:
+    Node* connect(Node* root) {
+        if(!root){
+            return root;
+        }
+        queue<Node*> q;
+        q.push(root);
+        while(q.empty()==false){
+            int sze=q.size();
+            for(int i=0;i<sze;i++){
+                Node* curr=q.front();
+                if(curr->left){
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
+                q.pop();
+                if(i<sze-1){
+                    curr->next=q.front();
+                }
+                else if(i==sze-1){
+                    curr->next=NULL;
+                }
+            }
+        }
+        return root;
+    }
+};
