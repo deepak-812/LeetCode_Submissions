688d7972c960c1497685c8eb788300c0aaaae711
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2022-09-01 17:37:31 +0530

    Cpp solution

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {  // O(H) T.C.  O(n) A.S. O(H) (call stack space)
+public:
+    int cnt;
+    int goodNodes(TreeNode* root) {
+        cnt=0;
+        solve(root,INT_MIN);
+        return cnt;
+    }
+
+    void solve(TreeNode* root,int maxVal){
+        if(!root){
+            return;
+        }
+
+        if(root->val>=maxVal){
+            cnt++;
+            maxVal=root->val;
+        }
+
+        solve(root->left,maxVal);
+        solve(root->right,maxVal);
+
+        return;
+    }
+
+};
