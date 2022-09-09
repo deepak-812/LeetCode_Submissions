3a185980e434309f76bcf229a016f3207eb9b5b3
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2022-09-09 10:55:59 +0530

    Time: 40 ms (48.65%), Space: 126.3 MB (53.39%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ 
+class Solution { // T.C. O(n^2)  A.S. O(h)
+public:
+    int preind;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        preind=0;
+        return makeTree(preorder,inorder,0,preorder.size()-1);
+    }
+    
+    TreeNode* makeTree(vector<int>& preorder,vector<int>& inorder,int is,int ie){  // O(n^2) T.C. O(h) for call stacks
+        if(is>ie){
+            return NULL;
+        }
+        
+        TreeNode* root=new TreeNode(preorder[preind++]);
+        
+        int inInd;
+        for(int i=is;i<=ie;i++){
+            if(inorder[i]==root->val){
+                inInd=i;
+                break;
+            }
+        }
+        
+        
+        
+        root->left=makeTree(preorder,inorder,is,inInd-1);
+        root->right=makeTree(preorder,inorder,inInd+1,ie);
+        return root;
+    }
+    
+};
+*/
+
+class Solution {  // T.C. O(n) and A.S. O(h)+O(n) O(h) for DFS and O(n) for Hashmap
+public:
+    int preind;
+    unordered_map<int,int> mp;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        preind=0;
+        for(int i=0;i<inorder.size();i++){ // O(n)
+            mp[inorder[i]]=i;
+        }
+        return makeTree(preorder,inorder,0,preorder.size()-1);
+    }
+    
+    TreeNode* makeTree(vector<int>& preorder,vector<int>& inorder,int is,int ie){  // O(n^2) T.C. O(h) for call stacks
+        if(is>ie){
+            return NULL;
+        }
+        
+        TreeNode* root=new TreeNode(preorder[preind++]);
+        
+        
+        int inInd=mp[root->val];
+        
+        
+        root->left=makeTree(preorder,inorder,is,inInd-1);
+        root->right=makeTree(preorder,inorder,inInd+1,ie);
+        return root;
+    }
+    
+};
