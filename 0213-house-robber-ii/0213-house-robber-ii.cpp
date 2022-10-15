27d91a3f6d11f857346f028c547674c1467c95c4
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2022-10-15 23:26:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,10 +1,10 @@
-class Solution {
+class Solution { // T.C. O(n) and A.S. O(1)
 public:
-    int rob(vector<int>& nums) {
+    int solve(vector<int> & nums){
         int n=nums.size();
         int prev=nums[0];
         int prev2=0;
-        for(int i=1;i<n-1;i++){
+        for(int i=1;i<n;i++){
             int take=nums[i];
             if(i>1){
                 take+=prev2;
@@ -12,20 +12,21 @@ public:
             int nottake=prev;
             prev2=prev , prev=max(take,nottake);
         }
-        int ans=prev;
-        if(n==1){
-            return prev;
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
         }
-        prev=nums[1];
-        prev2=0;
-        for(int i=2;i<n;i++){
-            int take=nums[i];
-            if(i>1){
-                take+=prev2;
+        vector<int> arr1,arr2;
+        for(int i=0;i<nums.size();i++){
+            if(i!=0){
+                arr1.push_back(nums[i]);
             }
-            int nottake=prev;
-            prev2=prev , prev=max(take,nottake);
+            if(i!=nums.size()-1){
+                arr2.push_back(nums[i]);
+            }
         }
-        return max(ans,prev);
+        return max(solve(arr1),solve(arr2));
     }
 };
