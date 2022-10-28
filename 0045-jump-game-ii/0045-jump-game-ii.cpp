77b975ea31a5444cc20d3d3abcfa485764696e3e
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2022-10-28 11:34:31 +0000

    Time: 527 ms (31.13%), Space: 17.9 MB (12.44%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        dp[nums.size()-1]=0;
+        return solve(nums,0,nums.size()-1,dp);
+    }
+    
+    int solve(vector<int> &nums,int ind,int lastInd,vector<int> &dp){
+        
+        if(ind==lastInd){
+           return 0;
+        }
+        
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        
+        int minMoves=10000;
+        for(int forward=1;forward<=nums[ind] && forward+ind<=lastInd;forward++){
+            minMoves=min(solve(nums,ind+forward,lastInd,dp)+1,minMoves);
+        }
+        return dp[ind]=minMoves; 
+    }
+    
+};
