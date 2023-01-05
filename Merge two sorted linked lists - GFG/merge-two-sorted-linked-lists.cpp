e917d53a0adbfaca8464cb2c078aec67b1729c8f
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2023-01-05 15:35:22 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to merge two sorted linked list.
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    // code here
+    if(!head1 && !head2){
+        return NULL;
+    }
+    if(!head2){
+        return head1;
+    }
+    if(!head1){
+        return head2;
+    }
+    Node* start=NULL,*last;
+    if(head1->data<head2->data){
+        start=last=head1;
+        head1=head1->next;
+    }
+    else{
+        start=last=head2;
+        head2=head2->next;
+    }
+    while(head1 && head2){
+        if(head1->data<head2->data){
+            last=last->next=head1;
+            head1=head1->next;
+        }
+        else{
+            last=last->next=head2;
+            head2=head2->next;
+        }
+    }
+    if(head2){
+        last->next=head2;
+    }
+    else if(head1){
+        last->next=head1;
+    }
+    return start;
+}  
