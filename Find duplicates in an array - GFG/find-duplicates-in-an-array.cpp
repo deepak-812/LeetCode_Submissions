ed9502d6caf4371285790b73f013fe9f3fcd002b
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2023-01-17 12:00:20 +0530

    Added solution - LeetHub

---
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        // code here
+        unordered_map<int,int> freq;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(freq[arr[i]]==1){
+                ans.push_back(arr[i]);
+            }
+            freq[arr[i]]++;
+        }
+        if(!ans.size()){
+            ans.push_back(-1);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
