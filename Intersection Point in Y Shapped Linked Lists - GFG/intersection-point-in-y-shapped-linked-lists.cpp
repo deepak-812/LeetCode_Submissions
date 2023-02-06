89271915f4f130dca235e7859652d1e7f749da91
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2023-02-06 13:28:15 +0530

    Added solution - LeetHub

---
+++ Intersection Point in Y Shapped Linked Lists - GFG/intersection-point-in-y-shapped-linked-lists.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+#include<iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n1,n2,n3;
+
+    cin>>T;
+    while(T--)
+    {
+        cin>>n1>>n2>>n3;
+        
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+        
+        Node* temp = head1;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        temp = head2;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2){
+    // Your Code Here
+    int len1=0,len2=0;
+    Node* curr=head1;
+    while(curr){
+        len1++;
+        curr=curr->next;
+    }
+    curr=head2;
+    while(curr){
+        len2++;
+        curr=curr->next;
+    }
+    if(len1>len2){
+        for(int i=0;i<len1-len2;i++){
+            head1=head1->next;
+        }
+    }
+    else{
+        for(int i=0;i<len2-len1;i++){
+            head2=head2->next;
+        }
+    }
+    while(head1!=head2){
+        head1=head1->next;
+        head2=head2->next;
+    }
+    if(!head1)return -1;
+    return head1->data;
+}
+
