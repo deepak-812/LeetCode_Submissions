4038c9a2b20ebedf17fac773bdce2b4cca19278e
Deepak Sharma <104820894+deepak-812@users.noreply.github.com>
2023-02-24 16:49:46 +0530

    Added solution - LeetHub

---
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+private:
+    vector<int> col;
+    bool dfs(int node,int c,vector<int> adj[]){
+        col[node]=c;
+        for(int i=0;i<adj[node].size();i++){
+            if(col[adj[node][i]]==-1){
+                if(!dfs(adj[node][i],c^1,adj)){
+                    return false;
+                }
+            }
+            else if(col[adj[node][i]]==col[node]){
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+	bool isBipartite(int v, vector<int>adj[]){
+	    // Code here
+	   col.resize(v,-1);
+	    for(int i=0;i<v;i++){
+	        if(col[i]==-1){
+	            if(!dfs(i,0,adj))return false;
+	        }
+	    }
+	    return true;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isBipartite(V, adj);    
+		if(ans)cout << "1\n";
+		else cout << "0\n";  
+	}
+	return 0;
+}
+// } Driver Code Ends
